b9e82ea0154cfa7cbc45db42c3cbfc5e7f41885c
Steven Smith <stevensmith@Stevens-Air.netgear.com>
2016-03-21 13:36:37 -0600

    wrote fibanaci function

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ let multiples_sum =
     () => _.chain(1).range(1000).filter((num)=> num % 3 === 0 || num % 5 === 0).sum().value()
 
 console.log("The sum of all the multiples of 3 or 5 below 1000.", multiples_sum())
+
+let fibanaci = _.memoize((index) => {
+    if (index === 0) {
+        return 0
+    } else if (index === 1) {
+        return 1
+    } else {
+        return fibanaci(index - 1) + fibanaci(index - 2)
+    }
+})
+
+console.log(fibanaci(6))
